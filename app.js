e6bf652cbfa3f3f5cdaf59783fefc85c8fc01e88
Alexander <alexanderl96@yahoo.com>
2021-06-16 10:51:12 -0400

    updating to Heroku

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// middleware
+
+// Middleware
 app.use(express.json());
 app.use(cors());
+
 // Routes
 app.get("/", (req, res) => {
-    res.send("Welcome to the Budgetting App!");
+    res.send("Welcome to the Budget-Incomen App!");
   });
+
 module.exports = app;
